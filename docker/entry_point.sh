fb9d1074170b24d37306fd0e9872a52ae1d9cde9
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-10-11 01:18:31 +0900

    add app

---
+++ docker/entry_point.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd src
+composer install
+php artisan cache:clear
+php artisan config:clear
+chmod -R 777 storage
+php artisan migrate --force
+php artisan serve --host=0.0.0.0 --port=80
